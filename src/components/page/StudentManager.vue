36d6f1ad3ab82419bcb48e988511dbde2100591e
yuanxiaohu <125733004@qq.com>
2018-10-15 18:29:23 +0800

    学生管理界面

--- src/components/page/StudentManager.vue
+++ src/components/page/StudentManager.vue
@@ -6,15 +6,38 @@
             </el-breadcrumb>
         </div>
         <div class="container">
-            <div class="handle-box">
-                <el-button type="primary" icon="delete" class="handle-del mr10" @click="delAll">批量删除</el-button>
-                <el-select v-model="select_cate" placeholder="筛选省份" class="handle-select mr10">
-                    <el-option key="1" label="广东省" value="广东省"></el-option>
-                    <el-option key="2" label="湖南省" value="湖南省"></el-option>
-                </el-select>
-                <el-input v-model="select_word" placeholder="筛选关键词" class="handle-input mr10"></el-input>
-                <el-button type="primary" icon="search" @click="search">搜索</el-button>
-            </div>
+            <!-- <div class="handle-box">
+
+            </div> -->
+            <el-row type="flex" class="handle-box" justify="space-between">
+                <!-- <el-col  :span="21"> -->
+                <div>
+                    <el-button type="primary" icon="delete" class="handle-del mr10" @click="delAll">新增学生</el-button>
+                     <span>年级:</span>
+                    <el-select v-model="select_cate" placeholder="筛选省份" class="handle-select mr10">
+                        <el-option key="1" label="广东省" value="广东省"></el-option>
+                        <el-option key="2" label="湖南省" value="湖南省"></el-option>
+                    </el-select>
+                    <span>班级:</span>
+                    <el-select v-model="select_cate" placeholder="筛选省份" class="handle-select mr10">
+                        <el-option key="1" label="广东省" value="广东省"></el-option>
+                        <el-option key="2" label="湖南省" value="湖南省"></el-option>
+                    </el-select>
+                    <span>学生姓名:</span>
+                    <el-input v-model="select_word" placeholder="筛选关键词" class="handle-input mr10"></el-input>
+                    <span>学号:</span>
+                    <el-input v-model="select_word" placeholder="筛选关键词" class="handle-input mr10"></el-input>
+                    <el-button type="primary" icon="search" @click="search">搜索</el-button>
+                    <el-button  icon="search" @click="search">重置</el-button>
+                </div>
+                <!-- </el-col> -->
+                <!-- <el-col  :span="3"> -->
+                <div>
+                    <el-button type="primary" icon="delete" class="handle-del mr10" @click="delAll">批量删除</el-button>
+                    <el-button type="primary" icon="delete" class="handle-del mr10" @click="delAll">批量导入</el-button>
+                </div>
+                <!-- </el-col> -->
+            </el-row>
             <el-table :data="data" border class="table" ref="multipleTable" @selection-change="handleSelectionChange">
                 <el-table-column type="selection" width="55" align="center"></el-table-column>
                 <el-table-column prop="date" label="日期" sortable width="150">
@@ -68,145 +91,145 @@
 </template>
 
 <script>
-    export default {
-        name: 'studentManager',
+export default {
+    name: 'studentManager',
+    data() {
+        return {
+            url: './static/vuetable.json',
+            tableData: [],
+            cur_page: 1,
+            multipleSelection: [],
+            select_cate: '',
+            select_word: '',
+            del_list: [],
+            is_search: false,
+            editVisible: false,
+            delVisible: false,
+            form: {
+                name: '',
+                date: '',
+                address: ''
+            },
+            idx: -1
+        }
+    },
+    created() {
+        this.getData();
+    },
+    computed: {
         data() {
-            return {
-                url: './static/vuetable.json',
-                tableData: [],
-                cur_page: 1,
-                multipleSelection: [],
-                select_cate: '',
-                select_word: '',
-                del_list: [],
-                is_search: false,
-                editVisible: false,
-                delVisible: false,
-                form: {
-                    name: '',
-                    date: '',
-                    address: ''
-                },
-                idx: -1
-            }
-        },
-        created() {
+            return this.tableData.filter((d) => {
+                let is_del = false;
+                for (let i = 0; i < this.del_list.length; i++) {
+                    if (d.name === this.del_list[i].name) {
+                        is_del = true;
+                        break;
+                    }
+                }
+                if (!is_del) {
+                    if (d.address.indexOf(this.select_cate) > -1 &&
+                        (d.name.indexOf(this.select_word) > -1 ||
+                            d.address.indexOf(this.select_word) > -1)
+                    ) {
+                        return d;
+                    }
+                }
+            })
+        }
+    },
+    methods: {
+        // 分页导航
+        handleCurrentChange(val) {
+            this.cur_page = val;
             this.getData();
         },
-        computed: {
-            data() {
-                return this.tableData.filter((d) => {
-                    let is_del = false;
-                    for (let i = 0; i < this.del_list.length; i++) {
-                        if (d.name === this.del_list[i].name) {
-                            is_del = true;
-                            break;
-                        }
-                    }
-                    if (!is_del) {
-                        if (d.address.indexOf(this.select_cate) > -1 &&
-                            (d.name.indexOf(this.select_word) > -1 ||
-                                d.address.indexOf(this.select_word) > -1)
-                        ) {
-                            return d;
-                        }
-                    }
-                })
-            }
+        // 获取 easy-mock 的模拟数据
+        getData() {
+            // 开发环境使用 easy-mock 数据，正式环境使用 json 文件
+            if (process.env.NODE_ENV === 'development') {
+                this.url = '/ms/table/list';
+            };
+            this.$axios.post(this.url, {
+                page: this.cur_page
+            }).then((res) => {
+                this.tableData = res.data.list;
+            })
         },
-        methods: {
-            // 分页导航
-            handleCurrentChange(val) {
-                this.cur_page = val;
-                this.getData();
-            },
-            // 获取 easy-mock 的模拟数据
-            getData() {
-                // 开发环境使用 easy-mock 数据，正式环境使用 json 文件
-                if (process.env.NODE_ENV === 'development') {
-                    this.url = '/ms/table/list';
-                };
-                this.$axios.post(this.url, {
-                    page: this.cur_page
-                }).then((res) => {
-                    this.tableData = res.data.list;
-                })
-            },
-            search() {
-                this.is_search = true;
-            },
-            formatter(row, column) {
-                return row.address;
-            },
-            filterTag(value, row) {
-                return row.tag === value;
-            },
-            handleEdit(index, row) {
-                this.idx = index;
-                const item = this.tableData[index];
-                this.form = {
-                    name: item.name,
-                    date: item.date,
-                    address: item.address
-                }
-                this.editVisible = true;
-            },
-            handleDelete(index, row) {
-                this.idx = index;
-                this.delVisible = true;
-            },
-            delAll() {
-                const length = this.multipleSelection.length;
-                let str = '';
-                this.del_list = this.del_list.concat(this.multipleSelection);
-                for (let i = 0; i < length; i++) {
-                    str += this.multipleSelection[i].name + ' ';
-                }
-                this.$message.error('删除了' + str);
-                this.multipleSelection = [];
-            },
-            handleSelectionChange(val) {
-                this.multipleSelection = val;
-            },
-            // 保存编辑
-            saveEdit() {
-                this.$set(this.tableData, this.idx, this.form);
-                this.editVisible = false;
-                this.$message.success(`修改第 ${this.idx+1} 行成功`);
-            },
-            // 确定删除
-            deleteRow(){
-                this.tableData.splice(this.idx, 1);
-                this.$message.success('删除成功');
-                this.delVisible = false;
+        search() {
+            this.is_search = true;
+        },
+        formatter(row, column) {
+            return row.address;
+        },
+        filterTag(value, row) {
+            return row.tag === value;
+        },
+        handleEdit(index, row) {
+            this.idx = index;
+            const item = this.tableData[index];
+            this.form = {
+                name: item.name,
+                date: item.date,
+                address: item.address
             }
+            this.editVisible = true;
+        },
+        handleDelete(index, row) {
+            this.idx = index;
+            this.delVisible = true;
+        },
+        delAll() {
+            const length = this.multipleSelection.length;
+            let str = '';
+            this.del_list = this.del_list.concat(this.multipleSelection);
+            for (let i = 0; i < length; i++) {
+                str += this.multipleSelection[i].name + ' ';
+            }
+            this.$message.error('删除了' + str);
+            this.multipleSelection = [];
+        },
+        handleSelectionChange(val) {
+            this.multipleSelection = val;
+        },
+        // 保存编辑
+        saveEdit() {
+            this.$set(this.tableData, this.idx, this.form);
+            this.editVisible = false;
+            this.$message.success(`修改第 ${this.idx + 1} 行成功`);
+        },
+        // 确定删除
+        deleteRow() {
+            this.tableData.splice(this.idx, 1);
+            this.$message.success('删除成功');
+            this.delVisible = false;
         }
     }
+}
 
 </script>
 
-<style scoped>
-    .handle-box {
-        margin-bottom: 20px;
-    }
+<style less='lang' scoped>
+.handle-box {
+  margin-bottom: 20px;
+}
 
-    .handle-select {
-        width: 120px;
-    }
+.handle-select {
+  width: 120px;
+}
 
-    .handle-input {
-        width: 300px;
-        display: inline-block;
-    }
-    .del-dialog-cnt{
-        font-size: 16px;
-        text-align: center
-    }
-    .table{
-        width: 100%;
-        font-size: 14px;
-    }
-    .red{
-        color: #ff0000;
-    }
+.handle-input {
+  width: 150px;
+  display: inline-block;
+}
+.del-dialog-cnt {
+  font-size: 16px;
+  text-align: center;
+}
+.table {
+  width: 100%;
+  font-size: 14px;
+}
+.red {
+  color: #ff0000;
+}
 </style>
